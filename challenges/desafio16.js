a464e583e909ed5824ef7686271eeef5a2b63f4c
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-10-22 12:51:15 -0300

    req 16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,1 +1,6 @@
-// db.produtos
+db.produtos
+.updateMany({ nome: { $eq: "Big Mac" } },
+{ $currentDate: { ultimaModificacao: { $type: "date" } } });
+
+db.produtos
+.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });

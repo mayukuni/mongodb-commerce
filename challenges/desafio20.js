1471be22c3fcec3149f5c6e33a591c1fd380faa9
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-10-22 13:10:57 -0300

    req 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,5 @@
-// db.produtos
+db.produtos
+.updateMany({ nome: { $eq: "Quarteirão com Queijo" } }, { $pop: { ingredientes: -1 } });
+
+db.produtos
+.find({}, { nome: 1, ingredientes: 1, _id: 0 });

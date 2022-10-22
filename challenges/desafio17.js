074272b6ab1c9f0beecc647ffa7c6aca66ec3e43
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-10-22 12:56:57 -0300

    req 17

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,1 +1,5 @@
-// db.produtos
+db.resumoProdutos
+.insertOne({ franquia: "McDonalds", totalProdutos: db.produtos.count() });
+
+db.resumoProdutos
+.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });

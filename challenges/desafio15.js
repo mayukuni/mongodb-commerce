5a7bc16518b8aa8362293c2ffaa96d0e1331d3ce
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-10-22 12:39:07 -0300

    req 15

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,1 +1,11 @@
-// db.produtos
+db.produtos
+.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
+
+db.produtos
+.updateMany({ tags: { $in: ["bovino"] } }, { $inc: { avaliacao: 5 } });
+
+db.produtos
+.updateMany({ tags: { $in: ["ave"] } }, { $inc: { avaliacao: 3 } });
+
+db.produtos
+.find({}, { nome: 1, avaliacao: 1, _id: 0 });
